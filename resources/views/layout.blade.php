777930fe6db34d361bff0107d21f1b10e7923dc9
Dmitriy Primechaev <redlse@vk.com>
2021-12-15 12:27:58 +0300

    controller actions and new views

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta name="theme-color" content="#7952b3">
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="https://bootstrap-4.ru/docs/5.1/examples/checkout/form-validation.css" rel="stylesheet">
+</head>
+<body class="bg-light">
+
+<div class="container">
+    <h1>@yield('title')</h1>
+    <main>
+        @yield('content')
+    </main>
+</div>
+
+
+<script src="https://bootstrap-4.ru/docs/5.1/examples/checkout/form-validation.js"></script>
+</body>
+</html>
